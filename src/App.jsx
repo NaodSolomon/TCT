71f533fdcd70e04b052368332696abafccb8cb4f
Abenezer <abenezerlibamu421@gmail.com>
2026-02-25 20:19:21 +0300

    log and signin pages

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home.jsx"
+import Login from "./pages/Login.jsx"
+import Signup from "./pages/Signup.jsx"
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/home' element={<Home />} />
+      <Route path='*' element={<div>Page Not Found</div>} />
+    </Routes>
+  )
+}
+
+export default App
